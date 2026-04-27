b1d7d060da61eafe05a046bbe160861040e258dd
AgenteSimans <gabrielvnrb@hotmail.com>
2026-04-27 02:52:38 -0300

    ajuste nos pulos

--- Editado/assembler/i.asm
+++ Editado/assembler/i.asm
@@ -7,27 +7,28 @@ lw x7, 24(x0)
 lw x6, 28(x0) 
 lw x10, 32(x0)
 lw x3, 4(x1) 
-beq x3,x9,16
-beq x3,x8,24
-beq x3,x7,40
-beq x3,x6,52
-beq x0, x0, 72
+beq x3,x9,20
+beq x3,x8,28
+beq x3,x7,44
+beq x3,x6,60
+beq x0, x0, 76
 lw x3,4(x1) 
 beq x3,x15,-60
 beq x0,x0,-8
 lw x3, 4(x1)
-beq x3,x15,4
+beq x3,x15,8
 beq x0,x0,-8
 add x4,x0,x0
-beq x0,x0,36
+beq x0,x0,44
 lw x3, 4(x1)
-beq x3,x15,4
+beq x3,x15,8
 beq x0,x0,-8
 sub x4,x4,x10
-beq x0,x0,16
+beq x0,x0,24
 lw x3, 4(x1)
-beq x3,x15,4
+beq x3,x15,8
 beq x0,x0,-8
 add x4,x4,x10
-sw x4,8(x1) #leds
-beq x0,x0,-124
+beq x0,x0,4
+sw x4,8(x1) 
+beq x0,x0,-100
